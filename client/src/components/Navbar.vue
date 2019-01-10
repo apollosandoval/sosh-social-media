55b38e6456200b50e488699954a1764fea0770bf
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-01-09 18:58:14 -0700

    added navigation-drawer

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,25 @@
 <template>
-  <v-toolbar dark color="primary">
-    <v-toolbar-title>Sosh</v-toolbar-title>
+  <nav>
+    <v-toolbar dark color="primary" app>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Sosh</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
 
-  </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app color="primary">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      drawer: false,
+    }
+  }
 }
 </script>
 
